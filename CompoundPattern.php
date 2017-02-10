ab8503e72dfae10a7bfd4482b6fd91cbec077841
Jasper Stone <jasperwstone@gmail.com>
2017-02-10 21:55:29 +0000

    Adding goose and adapter. Referencing #11

--- CompoundPattern.php
+++ CompoundPattern.php
@@ -24,14 +24,26 @@ class RubberDuck implements Quackable{
     public function quack() { print "Squeak\n"; }
 }
 
+class Goose{
+    public function honk() { print "Honk\n"; }
+}
+
+class GooseAdapter implements Quackable{
+    private $goose;
+    public function __construct($goose){ $this->goose = $goose; }
+    public function quack(){ $this->goose->honk(); }
+}
+
 function simulate($duck) { $duck->quack(); } 
 
 $mallardDuck = new MallardDuck();
 $redheadDuck = new RedheadDuck();
 $duckCall = new DuckCall();
 $rubberDuck = new RubberDuck();
+$gooseDuck = new GooseAdapter(new Goose());
 
 simulate($mallardDuck);
 simulate($redheadDuck);
 simulate($duckCall);
 simulate($rubberDuck);
+simulate($gooseDuck);
