b5e5f6aa354ccc7c6d3aa19ad4df7528990e31aa
Jasper Stone <jasperwstone@gmail.com>
2017-02-12 16:48:54 +0000

    Adding the Quackologist observer. Referencing #11

--- CompoundPattern.php
+++ CompoundPattern.php
@@ -1,37 +1,109 @@
 <?php
 /**
  * Ported from Head First Design Patters Examples: http://www.headfirstlabs.com/books/hfdp/
- * Compound Pattern - 
+ * Compound Pattern - Combines two or more patterns into a solution that solves
+ * a recurring or general problem.
+ * 
+ * p. 522
+ * Question 1: So this was a compound pattern?
+ * Answer: No, this was just a set of patterns working together. A compound
+ * pattern is a set of a few patterns that are combined to solve a general
+ * problem... the Model-View-Controller (MVC) pattern is a collection of a few
+ * patterns that has been used over and over in many design solutions.
  */
 
-interface Quackable{
+interface Observer{
+    public function update($duck);
+}
+
+class Quackologist implements Observer{
+    public function update($duck) { print "Quackologist: " . $duck . " just quacked.\n"; }
+}
+
+interface QuackObservable{
+    public function registerObserver($observer);
+    public function notifyObservers();
+}
+
+interface Quackable extends QuackObservable{
     public function quack();
 }
 
+class Observable implements QuackObservable{
+    private $observers = array();
+    private $duck;
+    public function __construct($duck) { $this->duck = $duck; }
+    public function registerObserver($observer) { $this->observers[] = $observer; }
+    public function notifyObservers(){
+        $ao = new ArrayObject($this->observers);
+        $iterator = $ao->getIterator();
+        while($iterator->valid()) {
+            $iterator->current()->update($this->duck);
+            $iterator->next();
+        }
+    }
+}
+
 class MallardDuck implements Quackable{
-    public function quack() { print "Quack\n"; }
+    private $observable;
+    public function __construct() { $this->observable = new Observable($this); }
+    public function quack() {
+        print "Quack\n";
+        $this->notifyObservers();
+    }
+    public function registerObserver($observer) { $this->observable->registerObserver($observer); }
+    public function notifyObservers() { $this->observable->notifyObservers(); }
+    public function __toString() { return "Mallard Duck"; }
 }
 
 class RedheadDuck implements Quackable{
-    public function quack() { print "Quack\n"; }
+    private $observable;
+    public function __construct() { $this->observable = new Observable($this); }
+    public function quack() {
+        print "Quack\n";
+        $this->notifyObservers();
+    }
+    public function registerObserver($observer) { $this->observable->registerObserver($observer); }
+    public function notifyObservers() { $this->observable->notifyObservers(); }
+    public function __toString() { return "Redhead Duck"; }
 }
 
 class DuckCall implements Quackable{
-    public function quack() { print "Kwak\n"; }
+    private $observable;
+    public function __construct() { $this->observable = new Observable($this); }
+    public function quack() {
+        print "Kwak\n";
+        $this->notifyObservers();
+    }
+    public function registerObserver($observer) { $this->observable->registerObserver($observer); }
+    public function notifyObservers() { $this->observable->notifyObservers(); }
+    public function __toString() { return "Duck Call"; }
 }
 
 class RubberDuck implements Quackable{
-    public function quack() { print "Squeak\n"; }
+    private $observable;
+    public function __construct() { $this->observable = new Observable($this); }
+    public function quack() {
+        print "Squeak\n";
+        $this->notifyObservers();
+    }
+    public function registerObserver($observer) { $this->observable->registerObserver($observer); }
+    public function notifyObservers() { $this->observable->notifyObservers(); }
+    public function __toString() { return "Rubber Duck"; }
 }
 
 class Goose{
     public function honk() { print "Honk\n"; }
+    public function __toString() { return "Goose"; }
 }
 
 class GooseAdapter implements Quackable{
     private $goose;
-    public function __construct($goose){ $this->goose = $goose; }
+    private $observervable;
+    public function __construct($goose){ $this->goose = $goose; $this->observable = new Observable($this); }
     public function quack(){ $this->goose->honk(); }
+    public function registerObserver($observer) { $this->observable->registerObserver($observer); }
+    public function notifyObservers() { $this->observable->notifyObservers(); }
 }
 
 class QuackCounter implements Quackable{
@@ -40,6 +112,8 @@ class QuackCounter implements Quackable{
     public function __construct($duck){ $this->duck = $duck; }
     public function quack() { $this->duck->quack(); self::$numberOfQuacks++; }
     public static function numberOfQuacks() { return self::$numberOfQuacks; }
+    public function registerObserver($observer) { $this->duck->registerObserver($observer); }
+    public function notifyObservers() { $this->duck->notifyObservers(); }
 }
 
 abstract class AbstractDuckFactory{
@@ -71,9 +145,21 @@ class Flock implements Quackable{
         $ao = new ArrayObject($this->quackers);
         $iterator = $ao->getIterator();
         while($iterator->valid()) {
-            print $iterator->key() . ' => ' . $iterator->current()->quack();
+            $iterator->current()->quack();
             $iterator->next();
         }
+    }
+    public function registerObserver($observer){
+        $ao = new ArrayObject($this->quackers);
+        $iterator = $ao->getIterator();
+        while($iterator->valid()) {
+            $iterator->current()->registerObserver($observer);
+            $iterator->next();
+        }
+    }
+    public function notifyObservers(){
+        // Each Quackable does its own notification, so Flock doesn't have to worry about it
+        // This happens when Flock delegates quack() to each Quackable in the Flock
     }
 }
 
@@ -105,6 +191,10 @@ class DuckSimulator{
         $flockOfMallards->add($duckFactory->createMallardDuck());
         
         $flockOfDucks->add($flockOfMallards);
+        
+        print "\nDuck Simulator: With Observer\n";
+        $quackologist = new Quackologist();
+        $flockOfDucks->registerObserver($quackologist);
         
         print "\nDuck Simulator: Whole Flock Simulation\n";
         $this->simulateQuack($flockOfDucks);
